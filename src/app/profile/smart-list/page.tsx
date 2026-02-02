df1438e11dbb511526bc7ef076f2ef65eaf5243f
Islam Mohammad <elfrash10@gmail.com>
2026-02-02 20:07:19 +0200

    implement order-history&payment routes

---
+++ src/app/profile/smart-list/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SmartListPage() {
+  return (
+    <div>
+      <p className="text-center text-3xl font-semibold mt-[20%]">
+        Under development
+      </p>
+    </div>
+  );
+}
+
+export default SmartListPage;
